3860f7de97c341c353a215447126a056c28d03ad
Michael Hucka <mhucka@caltech.edu>
2017-11-07 20:48:50 -0800

    Add __init__.py

---
+++ locterms/__init__.py
@@ -0,0 +1,13 @@
+#
+# @file    __init__.py
+# @brief   CASICS LoCTerms package __init__ file
+# @author  Michael Hucka
+#
+# <!---------------------------------------------------------------------------
+# Copyright (C) 2015 by the California Institute of Technology.
+# This software is part of CASICS, the Comprehensive and Automated Software
+# Inventory Creation System.  For more information, visit http://casics.org.
+# ------------------------------------------------------------------------- -->
+
+from .query-locterms import *
+from .version import *
