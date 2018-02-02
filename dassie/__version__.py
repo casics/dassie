c1c3aa8f37eb019af1aea1748ce58b4df5e917dd
Michael Hucka <mhucka@caltech.edu>
2018-02-01 20:38:07 -0800

    Rename from LoCTerms to Dassie

--- dassie/__version__.py
+++ dassie/__version__.py
@@ -1,14 +1,14 @@
 # =============================================================================
 # @file    version.py
-# @brief   CASICS LoCTerms version info
+# @brief   CASICS Dassie version info
 # @author  Michael Hucka <mhucka@caltech.edu>
 # @license Please see the file named LICENSE in the project directory
-# @website https://github.com/casics/locterms
+# @website https://github.com/casics/dassie
 # =============================================================================
 
 __version__   = '1.0.0'
-__title__     = 'LoCTerms'
-__url__       = 'https://github.com/casics/locterms'
+__title__     = 'Dassie'
+__url__       = 'https://github.com/casics/dassie'
 __author__    = 'Michael Hucka <mhucka@caltech.edu>'
 __email__     = 'casics-team@googlegroups.com'
 __license__   = 'GNU General Public License, version 3.0'
